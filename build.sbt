5742f0e526ecb10ded7887da178760db9e3a0aa5
Jarek Ratajski <jratajski@gmail.com>
2016-01-19 20:30:50 +0100

    scalaz

--- build.sbt
+++ build.sbt
@@ -9,7 +9,8 @@ scalaVersion := "2.11.7"
 libraryDependencies ++= Seq(
   "com.typesafe.akka" % "akka-stream-experimental_2.11" % "2.0.2",
   "com.typesafe.akka" % "akka-http-core-experimental_2.11" % "2.0.2",
-  "com.typesafe.akka" % "akka-http-experimental_2.11" % "2.0.2" 
+  "com.typesafe.akka" % "akka-http-experimental_2.11" % "2.0.2" ,
+  "org.scalaz" %% "scalaz-core" % "7.2.0"
 )
 
 fork in run := true
--- src/main/scala/fun/reactive/fizzbuzz/FizzBuzz.scala
+++ src/main/scala/fun/reactive/fizzbuzz/FizzBuzz.scala
@@ -3,6 +3,8 @@ package fun.reactive.fizzbuzz
 import akka.actor.ActorSystem
 import akka.stream.{ClosedShape, ActorMaterializer}
 import akka.stream.scaladsl._
+import scalaz._
+import Scalaz._
 
 object FizzBuzz {
 
@@ -13,41 +15,38 @@ object FizzBuzz {
 
   def doItForMe() = {
     implicit val actorSystem = ActorSystem()
-    import actorSystem.dispatcher
     implicit val flowMaterializer = ActorMaterializer()
 
 
-
-    val g = RunnableGraph.fromGraph(GraphDSL.create() { implicit builder: GraphDSL.Builder[Unit] =>
+    val fizzbuzzGraph = RunnableGraph.fromGraph(GraphDSL.create() { implicit builder: GraphDSL.Builder[Unit] =>
       import GraphDSL.Implicits._
 
-      val inputSource = Source(1 to 100)
+      val inputSource = akka.stream.scaladsl.Source(1 to 100)
       val bcast = builder.add(Broadcast[Int](3))
+
       val merge = builder.add(ZipWith[Option[String], Option[String], String, String](
-        (a, b, c) => a.getOrElse("") + b.getOrElse(if (a.isDefined) {""} else c )
+        (a, b, c) => a | "" + ( b | (  (a.isDefined) ?  {""} | c ) )
       ))
 
 
       val fizzer = Flow[Int].map(x => if (x % 3 == 0) Some("Fizz") else None)
       val buzzer = Flow[Int].map(x => if (x % 5 == 0) Some("Buzz") else None)
       val stringify = Flow[Int].map(_.toString)
-      val out = Sink.foreach[String] { value =>
+      val out = akka.stream.scaladsl.Sink.foreach[String] { value =>
         println(value)
       }
 
-      inputSource ~> bcast ~> fizzer ~> merge.in0
-      bcast ~> buzzer ~> merge.in1
-      bcast ~> stringify ~> merge.in2
+      inputSource ~> bcast     ~> fizzer ~> merge.in0
+                 bcast ~> buzzer   ~> merge.in1
+                 bcast ~> stringify ~> merge.in2
 
       merge.out ~> out
-
-
 
       ClosedShape
     })
 
 
-    g.run()
+    fizzbuzzGraph.run()
 
 
   }
